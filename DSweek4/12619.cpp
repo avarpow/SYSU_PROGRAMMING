aeee526917b2407fed4e69e3cdc832289817dd7e
avarpow <648120201@qq.com>
2020-09-23 20:28:20 +0800

    modify c-array

--- DSweek4/12619.cpp
+++ DSweek4/12619.cpp
@@ -15,8 +15,8 @@ public:
     const T &front() const;
 
 private:
-    int size
-        s int top;
+    int sizes;
+    int top;
     int end;
     T a[100];
 };
@@ -48,15 +48,14 @@ Queue<T>::Queue(const Queue &rhs)
 template <typename T>
 const Queue<T> &Queue<T>::operator=(const Queue<T> &rhs)
 {
-    Queue<T> t;
-    t.sizes = rhs.sizes;
-    t.top = rhs.top;
-    t.end = rhs.end;
+    sizes = rhs.sizes;
+    top = rhs.top;
+    end = rhs.end;
     for (int i = 0; i < 100; i++)
     {
-        (t.a)[i] = (rhs.a)[i];
+        a[i] = (rhs.a)[i];
     }
-    return t;
+    return *this;
 }
 
 template <typename T>
@@ -107,14 +106,16 @@ bool Queue<T>::pop()
 template <typename T>
 const T &Queue<T>::front() const
 {
-    if (empty())
-    {
-        return -1;
-    }
-    else
-    {
+
         return a[top];
-    }
+
+}
+#include <string>
+using std::string;
+template <typename T>
+string fun( Queue<T> &s){
+    s.push(123);
+    return string("fun exec");
 }
 
 #include <iostream>
@@ -125,6 +126,16 @@ int main()
     cout << "empty test " << s.empty() << "  size  " << s.size() << endl;
     cout << "push test " << s.push(100) << "  size  " << s.size() << endl;
     cout << "empty test " << s.empty() << "  size  " << s.size() << endl;
+    cout << "push test " << s.push(90) << "  size  " << s.size() << endl;
+    cout << "push test " << s.push(90) << "  size  " << s.size() << endl;
+    cout << "push test " << s.push(90) << "  size  " << s.size() << endl;
+    cout << "push test " << s.push(90) << "  size  " << s.size() << endl;
+    cout << "push test " << s.push(90) << "  size  " << s.size() << endl;
+    cout << "push test " << s.push(90) << "  size  " << s.size() << endl;
+    cout << "push test " << s.push(90) << "  size  " << s.size() << endl;
+    cout << "push test " << s.push(90) << "  size  " << s.size() << endl;
+    cout << "push test " << s.push(90) << "  size  " << s.size() << endl;
+    cout << "push test " << s.push(90) << "  size  " << s.size() << endl;
     cout << "push test " << s.push(90) << "  size  " << s.size() << endl;
     cout << "front test " << s.front() << "  size  " << s.size() << endl;
     cout << "pop test " << s.pop() << "  size  " << s.size() << endl;
@@ -138,15 +149,27 @@ int main()
     cout << "pop test " << s.pop() << "  size  " << s.size() << endl;
     cout << "pop test " << s.pop() << "  size  " << s.size() << endl;
     cout << "pop test " << s.pop() << "  size  " << s.size() << endl;
-    for (int i = 0; i < 200; i++)
-    {
-        cout << "push test " << s.push(2000 + i) << "  " << 2000 + i << "  "
-             << "  size  " << s.size() << endl;
-        cout << "push test " << s.push(1000 + i) << "  " << 1000 + i << "  "
-             << "  size  " << s.size() << endl;
-        cout << "front test " << s.front() << "  size  " << s.size() << endl;
-        cout << "pop test " << s.pop() << "  size  " << s.size() << endl;
-        cout << "front test " << s.front() << "  size  " << s.size() << endl;
-    }
+
+    //cout << "front test " << s.front() << "  size  " << s.size() << endl;
+    Queue<int> s2,s3;
+    //s2=s;
+    s3=s2=s;
+    cout << "front test dwdd   " << s.front() << "  size  " << s.size() << endl;
+    cout << "front test iiii   " << s2.front() << "  size  " << s2.size() << endl;
+    cout << "front test kkkkk   " << s3.front() << "  size  " << s3.size() << endl;
+    
     return 0;
 }
+/*
+队列的循环数组实现 实时评测
+ 2020-09-23 18:00
+ 1000 ms
+ 32 MB
+ 乔海燕 (qiaohy@mail.sysu.edu.cn)
+用循环数组实现队列，定义见Queue.h。
+
+注意：
+
+不可使用除数组和链表外的任何标准库工具如stack, queue,deque,vector和list等。
+可以使用长度为100的静态数组。 3 . 请提交整个实现，包括模板类的定义和实现。
+*/
